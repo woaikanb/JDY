d87bbd1d7392bd3e9200ae6b82383cda6cff94c2
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-03-21 20:00:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,23 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+#删除自带argon
+rm -rf  feeds/luci/themes/luci-theme-argon
+
+#添加老竭力的argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+#### 修改主机名字，把YOU-R4A修改你喜欢的就行（不能纯数字或者使用中文）
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
+
+#### 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+#sed -i "s/OpenWrt /PandoraBox /g" package/lean/default-settings/files/zzz-default-settings
+
+#### 稳定版修改R21xxx+自己的名字
+sed -i 's/R22.3.13/For FMJYY23/g' package/lean/default-settings/files/zzz-default-settings
+
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
+
+sed -i 's/"CPU 性能优化调节"/"处理器性能优化调节设置"/g' feeds/luci/applications/luci-app-cpufreq/po/zh-cn/cpufreq.po
