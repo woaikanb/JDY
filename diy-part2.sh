cdcfebc488f4a5c185d18c62d6902fabaa4532b7
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-05-11 21:12:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,15 +13,19 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
+rm -rf  feeds/luci/themes/luci-theme-argon
 
-sed -i "s/OpenWrt /PandoraBox/g" package/lean/default-settings/files/zzz-default-settings
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='YOY'' package/lean/default-settings/files/zzz-default-settings
+
+sed -i "s/OpenWrt /OPWRT/g" package/lean/default-settings/files/zzz-default-settings
 
 sed -i 's/R22.5.5/For FMJYY23/g' package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio0.ssid=PandoraBox/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio0.ssid=JDY/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-sed -i '/set wireless.default_radio0.ssid=PandoraBox/a\ set wireless.default_radio1.ssid=JDY' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio0.ssid=JDY/a\ set wireless.default_radio1.ssid=FCY' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
